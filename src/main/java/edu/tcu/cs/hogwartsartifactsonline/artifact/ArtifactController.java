2c6c9426637bba3b9eddceba3c183b62d288253a
Monito2005 <gustavocastillo885@gmail.com>
2026-03-21 20:31:51 -0500

    cotroller and service

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
@@ -0,0 +1,23 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.transform.Result;
+
+@RestController
+public class ArtifactController {
+
+    private final ArtifactService artifactService;
+
+    public ArtifactController(ArtifactService artifactService) {
+        this.artifactService = artifactService;
+    }
+
+    @GetMapping("/api/v1/artifacts/{artifactId}")
+    public Result findArtifactById(@PathVariable String artifactId) {
+        return null;
+    }
+
+}
