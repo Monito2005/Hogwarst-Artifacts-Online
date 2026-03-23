d36f51460fdcbc0d322a514ac1585e523e1d3e24
Monito2005 <gustavocastillo885@gmail.com>
2026-03-22 22:00:40 -0500

    test

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/dto/WizardDto.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/dto/WizardDto.java
@@ -3,4 +3,5 @@ package edu.tcu.cs.hogwartsartifactsonline.wizard.dto;
 public record WizardDto(Integer id,
                         String name,
                         Integer numberOfArtifacts) {
+
 }
